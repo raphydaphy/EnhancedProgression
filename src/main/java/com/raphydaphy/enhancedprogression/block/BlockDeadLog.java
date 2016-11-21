7169c1f24467342f2d585e8a0853a467a43b89ab
Raph Hennessy <raph.hennessy@gmail.com>
2016-11-22 09:47:34 +1100

    wow i am shit at making dead logs

--- src/main/java/com/raphydaphy/enhancedprogression/block/BlockDeadLog.java
+++ src/main/java/com/raphydaphy/enhancedprogression/block/BlockDeadLog.java
@@ -13,12 +13,11 @@ import net.minecraft.world.World;
 
 public class BlockDeadLog extends BlockBase
 {
-	private int frame = 0;
 	public static final PropertyInteger DECAY = PropertyInteger.create("decay", 1, 6);
 	public BlockDeadLog(String name) 
 	{
 		super(Material.WOOD, name);
-		this.setDefaultState(blockState.getBaseState().withProperty(DECAY, 1));
+		//this.setDefaultState(blockState.getBaseState().withProperty(DECAY, 1));
 		setHardness(1.0f);
 		setCreativeTab(EnhancedProgression.creativeTab);
 		this.setTickRandomly(true);
@@ -26,16 +25,21 @@ public class BlockDeadLog extends BlockBase
 	
 	public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state)
 	{
+		worldIn.setBlockState(pos, state.withProperty(DECAY, 0));
 		worldIn.scheduleBlockUpdate(pos, this, 10, 1);
 	}
+	
+	protected int getDecay(IBlockState state)
+    {
+        return ((Integer)state.getValue(DECAY)).intValue();
+    }
 	
 	@Override
 	public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand)
     {
-		if (frame < 6)
+		if (getDecay(state) < 6)
 		{
-			frame++;
-			worldIn.setBlockState(pos, state.withProperty(DECAY, frame));
+			worldIn.setBlockState(pos, state.withProperty(DECAY, getDecay(state) + 1));
 		}
     }
 	
