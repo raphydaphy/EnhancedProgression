74f98830023b924f8780876554f17ca18f8c9175
Raph Hennessy <raph.hennessy@gmail.com>
2016-11-21 23:23:33 +1100

    almost finished dead log

--- src/main/java/com/raphydaphy/enhancedprogression/block/BlockBase.java
+++ src/main/java/com/raphydaphy/enhancedprogression/block/BlockBase.java
@@ -1,11 +1,16 @@
 package com.raphydaphy.enhancedprogression.block;
 
+import java.util.Random;
+
 import com.raphydaphy.enhancedprogression.EnhancedProgression;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class BlockBase extends Block {
 
@@ -23,7 +28,9 @@ public class BlockBase extends Block {
 	public void registerItemModel(ItemBlock itemBlock) {
 		EnhancedProgression.proxy.registerItemRenderer(itemBlock, 0, name);
 	}
-
+	
+	public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) { }
+	
 	@Override
 	public BlockBase setCreativeTab(CreativeTabs tab) {
 		super.setCreativeTab(tab);
--- src/main/java/com/raphydaphy/enhancedprogression/block/BlockDeadLog.java
+++ src/main/java/com/raphydaphy/enhancedprogression/block/BlockDeadLog.java
@@ -1,22 +1,43 @@
 package com.raphydaphy.enhancedprogression.block;
 
+import java.util.Random;
+
 import com.raphydaphy.enhancedprogression.EnhancedProgression;
 
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class BlockDeadLog extends BlockBase
 {
+	private int frame = 0;
 	public static final PropertyInteger DECAY = PropertyInteger.create("decay", 1, 6);
 	public BlockDeadLog(String name) 
 	{
 		super(Material.WOOD, name);
-		this.setDefaultState(blockState.getBaseState().withProperty(DECAY, 6));
+		this.setDefaultState(blockState.getBaseState().withProperty(DECAY, 1));
 		setHardness(1.0f);
 		setCreativeTab(EnhancedProgression.creativeTab);
+		this.setTickRandomly(true);
 	}
+	
+	public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state)
+	{
+		worldIn.scheduleBlockUpdate(pos, this, 10, 1);
+	}
+	
+	@Override
+	public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand)
+    {
+		if (frame < 6)
+		{
+			frame++;
+			worldIn.setBlockState(pos, state.withProperty(DECAY, frame));
+		}
+    }
 	
 	@Override
 	public BlockStateContainer createBlockState()
