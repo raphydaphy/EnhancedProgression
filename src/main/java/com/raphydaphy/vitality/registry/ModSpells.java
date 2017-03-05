d6b2632220182175b05eb268c58e25674d04caee
RAPH HENNESSY <raph.hennessy@gmail.com>
2017-03-05 19:54:25 +1100

    fix new spells

--- src/main/java/com/raphydaphy/vitality/registry/ModSpells.java
+++ src/main/java/com/raphydaphy/vitality/registry/ModSpells.java
@@ -1,6 +1,8 @@
 package com.raphydaphy.vitality.registry;
 
 import com.raphydaphy.vitality.api.spell.Spell;
+import com.raphydaphy.vitality.spell.SpellExcavation;
+import com.raphydaphy.vitality.spell.SpellExplosion;
 import com.raphydaphy.vitality.spell.SpellFireball;
 import com.raphydaphy.vitality.spell.SpellIllumination;
 
@@ -10,10 +12,14 @@ public class ModSpells {
 
 	public static Spell FIREBALL;
 	public static Spell ILLUMINATION;
+	public static Spell EXPLOSION;
+	public static Spell EXCAVATION;
 	
 	public static void init(FMLInitializationEvent e){
 		FIREBALL = new SpellFireball();
 		ILLUMINATION = new SpellIllumination();
+		EXPLOSION = new SpellExplosion();
+		EXCAVATION = new SpellExcavation();
 	}
 	
 }
