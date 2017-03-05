38bc83012c9c9eafacc4ecc98bdaa0417b9b60ad
Shadows_of_Fire <Bward7864@gmail.com>
2017-03-04 21:42:54 -0500

    spel fix

--- src/main/java/com/raphydaphy/vitality/api/spell/SpellHelper.java
+++ src/main/java/com/raphydaphy/vitality/api/spell/SpellHelper.java
@@ -19,6 +19,7 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class SpellHelper {
 	// TODO: this class is shit but maps are too hard so rip
@@ -64,74 +65,63 @@ public class SpellHelper {
 		int potency = WandHelper.getCore(wand).getCoreType().getCost();
 
 		if (WandHelper.canUseEssence(wand, cost, WandHelper.getCore(wand).getCoreType())) {
-			if (caster.getEntityWorld().isRemote) {
 				Random rand = caster.getEntityWorld().rand;
 				for (int i = 0; i < 25; i++) {
 					double x = (double) (pos.getX()) + 0.5 + ((rand.nextDouble()) - 0.5);
-					double y = (double) pos.getY() + (rand.nextDouble()) + 0.5;
+					double y = (double) pos.getY() + (rand.nextDouble()) + 0.75;
 					double z = (double) pos.getZ() + 0.5 + ((rand.nextDouble()) - 0.5);
-					int[] pars = new int[1];
-					if (rand.nextInt(2) == 1) {
-						pars[0] = 165;
-					} else {
-						pars[0] = 133;
-					}
+					int pars = 133;
+					if (rand.nextInt(2) == 1) pars = 165;
 					caster.getEntityWorld().spawnParticle(EnumParticleTypes.FALLING_DUST, x, y, z, 0.0D, 0.0D, 0.0D,
 							pars);
 				}
-			} else {
 				WandHelper.useEssence(wand, cost, WandHelper.getCore(wand).getCoreType());
 				caster.getEntityWorld().playSound(null, pos, SoundEvents.BLOCK_GLASS_PLACE, SoundCategory.BLOCKS, 1, 1);
 				ItemStack stackToPlace = new ItemStack(ModBlocks.LIGHT_ORB);
 				stackToPlace.onItemUse(caster, caster.getEntityWorld(), pos, hand, facing, hitX, hitY, hitZ);
 				caster.swingArm(hand);
 				caster.getCooldownTracker().setCooldown(wand.getItem(), cooldown);
-			}
+			
 			return true;
-		} else {
+				
+		} else if (caster.getEntityWorld().isRemote){
 			ClientProxy.setActionText(I18n.format("vitality.wand.notenoughessence.name"),
 					WandHelper.getCore(wand).getCoreType().getColor());
-			return false;
 		}
+		return false;
 	}
 	
 	public static boolean fireballSpell(ItemStack wand, EntityPlayer caster) {
 		int cooldown = WandHelper.getCore(wand).getCoreType().getCooldown() * 2;
-		int cost = WandHelper.getCore(wand).getCoreType().getPotency();
-		int potency = WandHelper.getCore(wand).getCoreType().getCost() * 3;
-
+		int cost = WandHelper.getCore(wand).getCoreType().getPotency() * 3;
+		int potency = WandHelper.getCore(wand).getCoreType().getCost();
+		World world = caster.getEntityWorld();
 		if (WandHelper.canUseEssence(wand, cost, WandHelper.getCore(wand).getCoreType())) {
-			if (caster.getEntityWorld().isRemote) {
-				/*
+				BlockPos pos = caster.getPosition();
 				Random rand = caster.getEntityWorld().rand;
 				for (int i = 0; i < 25; i++) {
 					double x = (double) (pos.getX()) + 0.5 + ((rand.nextDouble()) - 0.5);
 					double y = (double) pos.getY() + (rand.nextDouble()) + 0.5;
 					double z = (double) pos.getZ() + 0.5 + ((rand.nextDouble()) - 0.5);
-					int[] pars = new int[1];
-					if (rand.nextInt(2) == 1) {
-						pars[0] = 165;
-					} else {
-						pars[0] = 133;
-					}
+					int pars = 165;
+					if (rand.nextInt(2) == 1) pars = 133;
+					
 					caster.getEntityWorld().spawnParticle(EnumParticleTypes.FALLING_DUST, x, y, z, 0.0D, 0.0D, 0.0D,
 							pars);
 				}
-				*/
-			} else {
+				
 				WandHelper.useEssence(wand, cost, WandHelper.getCore(wand).getCoreType());
-				caster.getEntityWorld().playSound(null, caster.getPosition(), SoundEvents.ENTITY_ENDERDRAGON_FIREBALL_EPLD, SoundCategory.BLOCKS, 1, 1);
+				world.playSound(null, caster.getPosition(), SoundEvents.ENTITY_ENDERDRAGON_FIREBALL_EPLD, SoundCategory.BLOCKS, 1, 1);
 				caster.getCooldownTracker().setCooldown(wand.getItem(), cooldown);
-				EntityLargeFireball bigBall = new EntityLargeFireball(caster.worldObj, caster, caster.getLookVec().xCoord, caster.getLookVec().yCoord, caster.getLookVec().zCoord);
+				EntityLargeFireball bigBall = new EntityLargeFireball(world, caster, caster.getLookVec().xCoord, caster.getLookVec().yCoord, caster.getLookVec().zCoord);
 				bigBall.accelerationX = caster.getLookVec().xCoord;
 				bigBall.accelerationY = caster.getLookVec().yCoord;
 				bigBall.accelerationZ = caster.getLookVec().zCoord;
 				bigBall.explosionPower = potency;
-				caster.worldObj.spawnEntityInWorld(bigBall);
+				if(!world.isRemote) world.spawnEntityInWorld(bigBall);
 				caster.getCooldownTracker().setCooldown(wand.getItem(), cooldown);
-			}
 			return true;
-		} else if (caster.getEntityWorld().isRemote){
+		} else if (world.isRemote){
 			
 				ClientProxy.setActionText(I18n.format("vitality.wand.notenoughessence.name"),
 					WandHelper.getCore(wand).getCoreType().getColor());
